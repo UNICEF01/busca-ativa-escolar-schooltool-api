d1b907392763f19e3154cbed07801774fbae3023
Sandy Santos <sandysantos@mycrosan.com.br>
2019-04-04 17:13:26 -0300

    Ajuste deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-ssh app@localhost -p 32769
+ssh -T app@localhost -p 32769
 cd projects/api.escolas.testes.buscaativaescolar.org.br
 bundle install --deployment --without development test
 bundle exec rake db:migrate RAILS_ENV=production
