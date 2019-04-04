ee7f7ee94b84960a3c0f5cd9559501cd17580f88
Sandy Santos <sandysantos@mycrosan.com.br>
2019-04-04 20:53:24 -0300

    Ajuste deploy 9

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 export PATH="/home/app/.rvm/rubies/ruby-2.4.1/bin/ruby"
-eval "$(rbenv init -)"
 bundle install --deployment --without development test
 bundle exec rake db:migrate RAILS_ENV=production
 passenger-config restart-app $(pwd)
