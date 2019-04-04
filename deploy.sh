1366cf8862e67417aae666772cbb7e83076095fc
Sandy Santos <sandysantos@mycrosan.com.br>
2019-04-04 19:49:44 -0300

    Ajuste deploy6

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-
-USERNAME=app
-HOST="localhost -p 32769"
-SCRIPT="cd projects/api.escolas.testes.buscaativaescolar.org.br; bundle install --deployment --without development test; bundle exec rake db:migrate RAILS_ENV=production; passenger-config restart-app $(pwd)"
-
-ssh -l USERNAME HOST SCRIPT
+#!/usr/bin/env bash
+bundle install --deployment --without development test
+bundle exec rake db:migrate RAILS_ENV=production
+passenger-config restart-app $(pwd)
