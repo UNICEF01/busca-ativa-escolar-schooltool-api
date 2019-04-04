93ad1541c95a429535fffec960745808c04590f6
Sandy Santos <sandysantos@mycrosan.com.br>
2019-04-04 20:04:36 -0300

    Ajuste deploy7

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
 bundle install --deployment --without development test
 bundle exec rake db:migrate RAILS_ENV=production
 passenger-config restart-app $(pwd)
