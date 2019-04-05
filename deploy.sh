c7e5a34b4f4c688d04e3fea3e7fbc86a12af97cb
Sandy Santos <sandysantos@mycrosan.com.br>
2019-04-04 22:53:24 -0300

    Ajuste deploy 14

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export PATH="/home/app/.rvm/gems/ruby-2.4.1/bin:/home/app/.rvm/gems/ruby-2.4.1@global/bin:/home/app/.rvm/rubies/ruby-2.4.1/bin:/home/app/bin:/home/app/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/home/app/.rvm/bin:/home/app/.rvm/bin"
+
 bundle install --deployment --without development test
 bundle exec rake db:migrate RAILS_ENV=production
 passenger-config restart-app $(pwd)
