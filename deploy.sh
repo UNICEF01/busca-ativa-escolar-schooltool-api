abea692f2ff1fc8f1e903fc382ac48fd01ccdc28
Sandy Santos <sandysantos@mycrosan.com.br>
2019-04-05 00:53:00 -0300

    Ajuste deploy 20

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-ssh app@localhost -p 32769 sh -c "cd projects/api.escolas.testes.buscaativaescolar.org.br; bundle install --deployment --without development test; bundle exec rake db:migrate RAILS_ENV=production; passenger-config restart-app $(pwd)"
+ssh app@localhost -p 32769 sh -c "cd projects/api.escolas.testes.buscaativaescolar.org.br; bundle install --deployment --without development test; bundle exec rake db:migrate RAILS_ENV=production; passenger-config restart-app $(pwd); $PATH"
 
 
