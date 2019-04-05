f3111133ce5e6351358e052a1f281cca1aeebef6
Sandy Santos <sandysantos@mycrosan.com.br>
2019-04-05 00:09:41 -0300

    Ajuste deploy 17

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+docker exec -it rnserverunicef bash
+su - app
+cd cd projects/api.escolas.testes.buscaativaescolar.org.br
 bundle install --deployment --without development test
 bundle exec rake db:migrate RAILS_ENV=production
 passenger-config restart-app $(pwd)
