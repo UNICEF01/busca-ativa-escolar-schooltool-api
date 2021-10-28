f4a0dcad99311c811cfbb0d1156896ef5039d6b7
Paulo <vieirajjr@gmail.com>
2021-10-28 10:56:07 -0300

    Removendo arquivos de teste

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,5 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('clientes', 'App\Http\Controllers\Api\BuscaSchoolApiController@index');
+
 //Route::get('alunos', 'App\Http\Controllers\Api\BuscaSchoolApiController@index');
-Route::apiResource('alunos', 'App\Http\Controllers\Api\BuscaSchoolApiController');
