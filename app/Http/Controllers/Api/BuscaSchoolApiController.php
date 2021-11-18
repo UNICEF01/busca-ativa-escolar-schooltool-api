552aad6a9879ccf26cb2f7d1fa47bbe2741f958b
Paulo <vieirajjr@gmail.com>
2021-11-18 09:06:31 -0300

    Adicionados comentarios organizacao do codigo

--- app/Http/Controllers/Api/BuscaSchoolApiController.php
+++ app/Http/Controllers/Api/BuscaSchoolApiController.php
@@ -37,7 +37,7 @@ class BuscaSchoolApiController extends Controller
             ->join('case_steps_alerta','children.id','=', 'case_steps_alerta.child_id')
             ->select(['case_steps_alerta.id','case_steps_alerta.name','case_steps_alerta.mother_name','case_steps_pesquisa.place_address','case_steps_alerta.place_cep','case_steps_pesquisa.place_reference','case_steps_pesquisa.place_neighborhood','children.educacenso_year','case_steps_pesquisa.school_last_name'])
             ->where([
-                ['schools.id', '=', $idescola]
+            ['schools.id', '=', $idescola]
             ])->get()->all();
         
             $final = [
@@ -46,74 +46,55 @@ class BuscaSchoolApiController extends Controller
                 "data" => $children,
                 "school" => $school
             ];
-
             return response()->json($final);
 
         } else {
-
             $final = [
                 "status" => "ERROR",
                 "message" => "Escola não localizada",
                 "data" => null,
                 "school" => null
             ];
-
-            return response()->json($final);
-               
+            return response()->json($final);              
         }
     }
 
     public function saveAlert(Request $request, $alert_id){
         
-         //1 - localizar alerta com base no id do alerta.
+         //1 - Localizar alerta com base no id do alerta.
         $alerta = Alerta::where(
             'id', '=', $alert_id
         )->get()->first();
+
         if( $alerta != null ){
-            
+            //2- Instanciar os campos a serem atualizados
             $place_address = $request->get("place_address");
             $place_cep = $request->get("place_cep");
             $place_reference = $request->get("place_reference");
             $place_neighborhood = $request->get("place_neighborhood");
     
-            //3 - Atualizar os campos do alerta.
+            //3 - Atualizar os campos instanciados no passo 2.
              $alerta->place_address = $place_address;
              $alerta->place_cep = $place_cep;
              $alerta->place_reference = $place_reference;
              $alerta->place_neighborhood = $place_neighborhood;
     
-            //4 - update
-             $alerta->save();
-            
-
-             $alerta = [
-               
+             $alerta->save();//4 - update
+        
+             $alerta = [//5 -Retorna o corpo do jason esperado no front
                 "status" => "SUCCESS",
                 "message" => "Alerta atualizado",
-                "data" => $alerta
+                "data" => $alerta  //data chama o alerta atualizado
             ];
-
             return response()->json($alerta);
 
-        } else {
-
+        } else {//Em caso do front enviar um id incorreto, o else retorna ERROR
             $alerta = [
                 "status" => "ERROR",
                 "message" => "Alerta não localizada",
                 "data" => null,
             ];
-
-            return response()->json($alerta);
-               
+            return response()->json($alerta);    
         }
-
-        //2 - Instanciar variáveis
-        //Pegar valor por meio de json return $request->get("place_neighborhood");
-
-       
-  
     }
-
-    
-
 }
