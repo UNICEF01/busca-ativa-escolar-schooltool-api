9c7f550997f2a3fb5c252808d53ba913aa70141c
Paulo <vieirajjr@gmail.com>
2021-11-23 18:31:55 -0300

    Correção de atualização dos campos no Alerta

--- app/Http/Controllers/Api/BuscaSchoolApiController.php
+++ app/Http/Controllers/Api/BuscaSchoolApiController.php
@@ -35,10 +35,12 @@ class BuscaSchoolApiController extends Controller
             ->join('case_steps_pesquisa','children.id', '=', 'case_steps_pesquisa.child_id')
             ->join('schools', 'case_steps_pesquisa.school_last_name', '=', 'schools.name')
             ->join('case_steps_alerta','children.id','=', 'case_steps_alerta.child_id')
-            ->select(['case_steps_alerta.id','case_steps_alerta.name','case_steps_alerta.mother_name','case_steps_pesquisa.place_address','case_steps_alerta.place_cep','case_steps_pesquisa.place_reference','case_steps_pesquisa.place_neighborhood','children.educacenso_year','case_steps_pesquisa.school_last_name'])
+            ->select(['case_steps_alerta.id','case_steps_alerta.name','case_steps_alerta.mother_name','case_steps_alerta.place_address','case_steps_alerta.place_cep','case_steps_alerta.place_reference','case_steps_alerta.place_neighborhood','children.educacenso_year','case_steps_pesquisa.school_last_name'])
             ->where([
             ['schools.id', '=', $idescola]
             ])->get()->all();
+
+
         
             $final = [
                 "status" => "SUCCESS",
